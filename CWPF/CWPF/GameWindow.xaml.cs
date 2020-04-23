65edfbda8e94863e4f7589de1df1a792e493fc29
Jonatan <Jonatandahl99@gmail.com>
2020-04-23 12:20:30 +0200

    Initial Commit

---
+++ CWPF/CWPF/GameWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CWPF
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        private Ellipse jumpingJona;
+        public GameWindow()
+        {
+            InitializeComponent();
+            IniJumpingJona();
+            //Thread thread = new Thread(new ThreadStart(MoveJumpingJona));
+            //thread.Start();
+        }
+
+
+        private void IniJumpingJona()
+        {
+            jumpingJona = new Ellipse();
+            jumpingJona.Name = "jumpingJona";
+            jumpingJona.Height = 50;
+            jumpingJona.Width = 50;
+            jumpingJona.StrokeThickness = 2;
+            jumpingJona.Fill = new SolidColorBrush(Colors.Blue);
+            jumpingJona.Stroke = new SolidColorBrush(Colors.Black);
+            jumpingJona.Margin = new Thickness(0, 0, 0, 0);
+            jonaGrid.Children.Add(jumpingJona);
+        }
+
+        private void MoveJumpingJona()
+        {
+            while (true)
+            {
+                if (Keyboard.IsKeyDown(Key.Right))
+                {
+                    Thickness currPos = jumpingJona.Margin;
+                    Thickness newPos = currPos;
+                    newPos.Right = newPos.Right + 100;
+                    jumpingJona.Margin = newPos;
+                }
+            }
+        }
+    }
+}
---
+++ CWPF/CWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            IniButtons();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
+        }
+
+        private void IniButtons()
+        {
+            Button startButton = new Button();
+            startButton.Height = 50;
+            startButton.Width = 80;
+            startButton.Content = "Start Game";
+            startButton.Background = new SolidColorBrush(Colors.Black);
+            startButton.Foreground = new SolidColorBrush(Colors.White);
+            startButton.Click += new RoutedEventHandler(startButton_Click);
+            startButton.Margin = new Thickness(0, 0, 0, 0);
+            buttonGrid.Children.Add(startButton);
+        }
+    }
+}
